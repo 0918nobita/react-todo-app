a87146a080e831aaea8f0425c6b3aec7fadffe57
0918nobita <nobita.0918@gmail.com>
2020-06-10 13:55:45 +0900

    :wrench: Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const distDir = path.resolve(__dirname, 'dist');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: './src/index.tsx',
+    output: {
+        filename: 'bundle.js',
+        path: distDir,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+    },
+    devServer: {
+        contentBase: distDir,
+        port: 3000,
+    },
+};
